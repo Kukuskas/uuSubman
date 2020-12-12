56fe511a70980d0c460ac47b692a4ae55358c5cb
Tatsiana Sarokina <tatsiana.sarokina@plus4u.net>
2020-12-13 00:14:15 +0100

    Ups :D

--- uu_subman_maing01-hi/src/routes/subjects.js
+++ uu_subman_maing01-hi/src/routes/subjects.js
@@ -4,11 +4,7 @@ import Config from "./config/config";
 import SubjectList from "../bricks/subject-list";
 import SubjectProvider from "../bricks/subject-provider";
 import SubjectCreate from "../bricks/subject-create";
-<<<<<<< HEAD
-import SubjectsTitle from "../bricks/subjects-title";
-=======
 import SubjectsTitle from "../bricks/subject-title";
->>>>>>> 57b05dfcd69ad21acaa2964dde1ef27ecb452184
 //@@viewOff:imports
 
 const Subjects = createVisualComponent({
@@ -16,27 +12,6 @@ const Subjects = createVisualComponent({
   displayName: Config.TAG + "Subjects",
   //@@viewOff:statics
 
-<<<<<<< HEAD
-  //@@viewOn:render
-render() {
- //@@viewOn:render
-return (
-  <UU5.Bricks.Container>
-    <SubjectProvider>
-      {({ viewState, subjects, handleCreate, handleDelete }) => {
-        return (
-          <>
-            <SubjectsTitle subjects={subjects} />
-            <SubjectCreate onCreate={handleCreate} />
-            <SubjectList subjects={subjects} onDelete={handleDelete} />
-          </>
-        );
-      }}
-    </SubjectProvider>
-  </UU5.Bricks.Container>
-);
-//@@viewOff:render
-=======
 //@@viewOn:render
 render() {
   //@@viewOn:render
@@ -56,7 +31,6 @@ render() {
     </UU5.Bricks.Container>
   );
   //@@viewOff:render
->>>>>>> 57b05dfcd69ad21acaa2964dde1ef27ecb452184
 }
 //@@viewOff:render
 });
