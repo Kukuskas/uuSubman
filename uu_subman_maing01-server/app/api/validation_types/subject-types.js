fc643a86444fc2fa09329743a5b511a4d2cb865d
Alexandr Kozlov <alexandr.kozlov@seznam.cz>
2020-12-14 17:51:45 +0100

    Subject-types.js corrected and calls to server working on full authentication

--- uu_subman_maing01-server/app/api/validation_types/subject-types.js
+++ uu_subman_maing01-server/app/api/validation_types/subject-types.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 const subjectCreateDtoInType = shape({
     name: shape({
-        cz: string(50).isRequired(),
+        cs: string(50).isRequired(),
         en: string(50).isRequired()
     }).isRequired(),
     credits: integer(10).isRequired(),
     supervisor: string(/([0-9]{3})[-]([0-9]{2})[-]([0-9]{3})/).isRequired(),
     degree: oneOf(["Bachalor", "Magister"]).isRequired(),
     desc: shape({
-        cz: string(500).isRequired(),
+        cs: string(500).isRequired(),
         en: string(500).isRequired()
     }).isRequired(),
     language: string(30).isRequired(),
