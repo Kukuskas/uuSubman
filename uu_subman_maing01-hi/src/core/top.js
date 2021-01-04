5258c2de85f8e0d3436940a8a79b7f20495d4306
Tatsiana Sarokina <tatsiana.sarokina@plus4u.net>
2021-01-04 21:02:18 +0100

    +top

--- uu_subman_maing01-hi/src/core/top.js
+++ uu_subman_maing01-hi/src/core/top.js
@@ -1,26 +1,26 @@
-// //@@viewOn:imports
-// import Plus4U5 from "uu_plus4u5g01";
-// import { createVisualComponent, useContext } from "uu5g04-hooks";
-// import Config from "./config/config";
-// import SubmanMainContext from "../bricks/subman-main-context";
-// //@@viewOff:imports
+//@@viewOn:imports
+import Plus4U5 from "uu_plus4u5g01";
+import { createVisualComponent, useContext } from "uu5g04-hooks";
+import Config from "./config/config";
+import SubmanMainContext from "../bricks/subman-main-context";
+//@@viewOff:imports
 
-// const Top = createVisualComponent({
-//   //@@viewOn:statics
-//   displayName: Config.TAG + "Top",
-//   //@@viewOff:statics
+const Top = createVisualComponent({
+  //@@viewOn:statics
+  displayName: Config.TAG + "Top",
+  //@@viewOff:statics
 
-//   render() {
-//     //@@viewOn:hooks
-//     const { data: SubmanMain } = useContext(SubmanMainContext);
-//     //@@viewOff:hooks
+  render() {
+    //@@viewOn:hooks
+    const { data: SubmanMain } = useContext(SubmanMainContext);
+    //@@viewOff:hooks
 
-//     //@@viewOn:render
-//     const title = `${SubmanMain.name} [${SubmanMain.state}]`;
+    //@@viewOn:render
+    const title = `${SubmanMain.name} [${SubmanMain.state}]`;
 
-//     return <Plus4U5.App.TopBt>{title}</Plus4U5.App.TopBt>;
-//     //@@viewOff:render
-//   }
-// });
+    return <Plus4U5.App.TopBt>{title}</Plus4U5.App.TopBt>;
+    //@@viewOff:render
+  }
+});
 
-// export default Top;
+export default Top;
