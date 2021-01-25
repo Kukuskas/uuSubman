fb83562ba958498292837836d3a16d8848ff623f
Tatsiana Sarokina <tatsiana.sarokina@plus4u.net>
2021-01-25 21:09:58 +0100

    works

--- uu_subman_maing01-hi/src/bricks/subject-list.js
+++ uu_subman_maing01-hi/src/bricks/subject-list.js
@@ -28,10 +28,10 @@ const SubjectList = createVisualComponent({
   defaultProps: {
     subjects: [],
     showButton: false,
-    onDetail: () => {},
-    onUpdate: () => {},
-    onDelete: () => {},
-    onCreate: () => {},
+    onDetail: () => { },
+    onUpdate: () => { },
+    onDelete: () => { },
+    onCreate: () => { },
   },
   //@@viewOff:defaultProps
 
@@ -57,13 +57,13 @@ const SubjectList = createVisualComponent({
     }
 
     function visibility() {
-      if (isAuthority || isAdministration) {
+      if (!isAuthority || !isAdministration) {
         subjects.map((data, index) => {
           const isGarant = data.data.supervisor === identity.uuIdentity;
           const isTeacher = data.data.teachers.some((teacher) => teacher === identity.uuIdentity);
           console.log(
-            "God mode: ", isAuthority, 
-            "Is administraton: ", isAdministration?true:false, 
+            "God mode: ", isAuthority,
+            "Is administraton: ", isAdministration ? true : false,
             "Is teacher: ", isTeacher || isGarant);
           if (!isGarant && !isTeacher && data.data.visibility == false) {
             subjects.splice(index, 1);
@@ -126,9 +126,6 @@ const SubjectList = createVisualComponent({
 
     //@@viewOn:render
 
-    if (subjects.length === 0) {
-      return <UU5.Common.Error content="WTF No subjects!" />;
-    }
 
     const GET_ACTIONS = ({ screenSize }) => {
       return [
@@ -145,6 +142,20 @@ const SubjectList = createVisualComponent({
         },
       ];
     };
+    if (subjects.length === 0) {
+      return <>
+        {console.log(isAuthority)}
+        <Uu5Tiles.ControllerProvider data={subjects}>
+          <Uu5Tiles.ActionBar actions={canManage() && showButton ? GET_ACTIONS : []} />
+        </Uu5Tiles.ControllerProvider>
+        <SubjectCreateForm
+          shown={showCreateModal}
+          onSave={handleCreateSubjectSave}
+          onCancel={handleCloseCreateSubjectForm}
+        />
+        <UU5.Common.Error content="WTF No subjects!" />
+      </>
+    }
 
     return (
       <>
@@ -155,7 +166,7 @@ const SubjectList = createVisualComponent({
         />
         {visibility() && (
           <Uu5Tiles.ControllerProvider data={subjects}>
-            {canManage() && <Uu5Tiles.ActionBar actions={showButton ? GET_ACTIONS : []} />}
+            <Uu5Tiles.ActionBar actions={canManage() && showButton ? GET_ACTIONS : []} />
             <Uu5Tiles.Grid tileHeight="auto" tileMinWidth={200} tileMaxWidth={300} tileSpacing={8} rowSpacing={8}>
               {renderItem}
             </Uu5Tiles.Grid>
