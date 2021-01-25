38b8e04201b6d52b81255f24d8836117c67bb09f
Alexandr Kozlov <alexandr.kozlov@seznam.cz>
2021-01-25 20:03:15 +0100

    Action bar fix

--- uu_subman_maing01-hi/src/bricks/subject-list.js
+++ uu_subman_maing01-hi/src/bricks/subject-list.js
@@ -53,24 +53,27 @@ const SubjectList = createVisualComponent({
       (profile) => profile === Config.Profiles.ADMINISTRATIONS
     );
     function canManage() {
-     return isAuthority || isAdministration
+      return isAuthority || isAdministration;
     }
 
     function visibility() {
-         if (!isAuthority || !isAdministration) {
+      if (!isAuthority || !isAdministration) {
         subjects.map((data, index) => {
           const isGarant = data.data.supervisor === identity.uuIdentity;
           const isTeacher = data.data.teachers.some((teacher) => teacher === identity.uuIdentity);
           console.log(
-            "God mode: ", isAuthority, 
-            "Is administraton: ", isAdministration?true:false, 
-            "Is teacher: ", isTeacher || isGarant);
+            "God mode: ",
+            isAuthority,
+            "Is administraton: ",
+            isAdministration ? true : false,
+            "Is teacher: ",
+            isTeacher || isGarant
+          );
           if (!isGarant && !isTeacher && data.data.visibility == false) {
             subjects.splice(index, 1);
           }
-        }
-        );
-       }
+        });
+      }
       return subjects;
     }
 
@@ -128,7 +131,12 @@ const SubjectList = createVisualComponent({
     //@@viewOn:render
 
     if (subjects.length === 0) {
-      return <UU5.Common.Error content="WTF No subjects!" />;
+      return (
+        <>
+          <Uu5Tiles.ActionBar actions={canManage() && showButton ? GET_ACTIONS : []} />
+          <UU5.Common.Error content="WTF No subjects!" />;
+        </>
+      );
     }
 
     const GET_ACTIONS = ({ screenSize }) => {
@@ -156,8 +164,7 @@ const SubjectList = createVisualComponent({
         />
         {visibility() && (
           <Uu5Tiles.ControllerProvider data={subjects}>
-           
-            <Uu5Tiles.ActionBar actions={  canManage() && showButton ? GET_ACTIONS : []} />
+            <Uu5Tiles.ActionBar actions={canManage() && showButton ? GET_ACTIONS : []} />
             <Uu5Tiles.Grid tileHeight="auto" tileMinWidth={200} tileMaxWidth={300} tileSpacing={8} rowSpacing={8}>
               {renderItem}
             </Uu5Tiles.Grid>
